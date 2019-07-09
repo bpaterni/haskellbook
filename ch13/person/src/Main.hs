38d9a6611684986cea58a26a247df6477f649d4d
Brian Paterni <bpaterni@gmail.com>
2019-07-09 00:03:18 -0500

    ch13: add 'person' project

---
+++ ch13/person/src/Main.hs
@@ -0,0 +1,53 @@
+module Main where
+
+import System.IO ( BufferMode(NoBuffering)
+                 , hSetBuffering
+                 , stdout )
+
+type Name = String
+type Age = Integer
+
+data Person = Person Name Age deriving Show
+
+data PersonInvalid =
+    NameEmpty
+  | AgeTooLow
+  | PersonInvalidUnknown String
+  deriving (Eq, Show)
+
+mkPerson :: Name
+         -> Age
+         -> Either PersonInvalid Person
+mkPerson name age
+  | name /= "" && age > 0 =
+  Right $ Person name age
+  | name == "" = Left NameEmpty
+  | not (age > 0) = Left AgeTooLow
+  | otherwise =
+    Left $ PersonInvalidUnknown $
+      "Name was: " ++ show name ++
+      " Age was: " ++ show age
+
+prependERROR :: String -> String
+prependERROR = ("ERROR: "++)
+
+personInvalidResult :: PersonInvalid -> String
+personInvalidResult NameEmpty = prependERROR . show $ NameEmpty
+personInvalidResult AgeTooLow = prependERROR . show $ AgeTooLow
+personInvalidResult (PersonInvalidUnknown s) = prependERROR s
+
+personResult :: Either PersonInvalid Person -> String
+personResult (Right _) = "Yay!"
+personResult (Left pi) = personInvalidResult pi
+
+gimmePerson :: IO ()
+gimmePerson = do
+  hSetBuffering stdout NoBuffering
+  putStr "Name: "
+  name <- getLine
+  putStr "Age: "
+  age <- getLine
+  putStrLn $ personResult (mkPerson name (read age))
+
+main :: IO ()
+main = gimmePerson
