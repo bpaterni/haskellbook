8f937e968b73991426786d332259d573c811b9cf
Brian Paterni <bpaterni@gmail.com>
2019-08-17 23:15:44 -0500

    ch20: add work up to section 4

---
+++ ch20/test/a03/A03.hs
@@ -0,0 +1,12 @@
+-- Revenge of the monoids
+import Data.Foldable
+import Data.Monoid
+
+--xs :: [Sum Integer]
+--xs :: [Product Integer]
+--xs = [1,2,3,4,5]
+
+xs = [Just 1, Nothing, Just 5]
+
+main :: IO ()
+main = putStrLn "Test suite not yet implemented"
