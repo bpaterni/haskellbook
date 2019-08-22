c9caaacd295c1b85d0b026b2bb0fd55b56eec7e9
Brian Paterni <bpaterni@gmail.com>
2019-08-21 22:48:35 -0500

    ch20: add remaining chapter exercises

--- ch20/test/a05/A05.hs
+++ ch20/test/a05/A05.hs
@@ -57,5 +57,68 @@ foldMap' :: (Foldable t, Monoid m)
          => (a -> m) -> t a  -> m
 foldMap' f = foldr ((<>) . f) mempty
 
+data Constant a b =
+  Constant b
+  deriving (Eq, Show)
+
+instance Foldable (Constant a) where
+  foldMap f (Constant b) = f b
+
+data Two a b = Two a b
+  deriving (Eq, Show)
+
+instance Functor (Two a) where
+  fmap f (Two a b) = Two a (f b)
+
+instance Monoid a => Applicative (Two a) where
+  pure x = Two mempty x
+  (Two a f) <*> (Two a' x) = Two (mappend a a') (f x)
+
+instance Foldable (Two a) where
+  foldMap f (Two _ b) = f b
+
+data Three a b c = Three a b c
+  deriving (Eq, Show)
+
+instance Functor (Three a b) where
+  fmap f (Three a b c) = Three a b (f c)
+
+instance Foldable (Three a b) where
+  foldMap f (Three _ _ c) = f c
+
+data Three' a b = Three' a b b
+  deriving (Eq, Show)
+
+instance Functor (Three' a) where
+  fmap f (Three' a b b') = Three' a (f b) (f b')
+
+instance (Monoid a)
+  => Applicative (Three' a) where
+    pure x = Three' mempty x x
+    Three' a f g <*> Three' a' x y = Three' (mappend a a') (f x) (g y)
+
+instance Foldable (Three' a) where
+  foldMap f (Three' a b b') = f b <> f b'
+
+data Four' a b = Four' a a a b
+  deriving (Eq, Show)
+
+instance Functor (Four' a) where
+  fmap f (Four' w x y z) = Four' w x y (f z)
+
+instance Monoid a
+  => Applicative (Four' a) where
+    pure x = Four' mempty mempty mempty x
+    Four' a b c f <*> Four' a' b' c' x = Four' (mappend a a') (mappend b b') (mappend c c') (f x)
+
+instance Foldable (Four' a) where
+  foldMap f (Four' _ _ _ b) = f b
+
+filterF :: ( Applicative f
+           , Foldable t
+           , Monoid (f a))
+           => (a -> Bool) -> t a -> f a
+filterF f = foldMap (\x -> if f x then pure x else mempty)
+
 main :: IO ()
 main = putStrLn "Test suite not yet implemented"
